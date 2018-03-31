74eebb167b365d232d5412790bb66d2a0a510a91
Sadeesha Eranga <msadeeshaeranga@gmail.com>
2018-03-31 22:44:19 +0530

    Add POST / todos route adn refractor mongoose

--- server/server.js
+++ server/server.js
@@ -1,60 +1,26 @@
-const mongoose = require('mongoose');
+let express = require('express');
+let bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
 
-let User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((result) => {
+        res.send(result);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-let newUser = new User({
-    email: 'msadeeshaeranga@gmail.com'
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
-
-newUser.save().then((result) => {
-    console.log('User saved', JSON.stringify(result, undefined, 4));
-}).catch((e) => {
-    console.log('Unable to save the user', e);
-})
-
-// let Todo = mongoose.model('Todo', {
-//     text: {
-//         type: String,
-//         required: true,
-//         minlength: 1,
-//         trim: true
-//     },
-//     completed: {
-//         type: Boolean,
-//         default: false
-//     },
-//     completedAt: {
-//         type: Number,
-//         default: null
-//     }
-// });
-
-// let newTodo = new Todo({
-//     text: 'Cook dinner'
-// });
-
-// newTodo.save().then((result) => {
-//     console.log('Todo saved', result);
-// }, (e) => {
-//     console.log('Unable to save the todo', e);
-// });
-
-// let myTodo = new Todo({
-//     text: '   Mogoose tutorials   '
-// });
-
-// myTodo.save().then((result) => {
-//     console.log('Todo saved', result);
-// }).catch((e) => {
-//     console.log('Unable to save todo', e);
-// });
